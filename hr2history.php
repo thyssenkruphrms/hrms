d51d143dc1cc5c1d4ce00aa8b02df5f588d6e459
Pragatigaikwad280 <35777215+Pragatigaikwad280@users.noreply.github.com>
2020-08-21 22:50:13 +0530

    hr2-profile nav changes

--- hr2history.php
+++ hr2history.php
@@ -11,6 +11,8 @@ if(isset($_COOKIE['sid']))
   {
     $cursor = $db->users->findOne(array("uid" => $cursor['uid']));
     $designation = $cursor['dsg'];
+    $name = $cursor['name'];
+
     
     if($designation == "hr2" || $designation == "ceo" || $designation == "hod" || $designation == "rghead" )
     {
@@ -37,6 +39,7 @@ if(isset($_COOKIE['sid']))
   
   <script src="./public/js/materialize.js"></script>
   <script src="./public/js/materialize.min.js"></script>
+  <script src="./public/js/logout.js"></script>
 
 <style>
 @media screen and (min-width: 800px)
@@ -111,6 +114,11 @@ width: 350%;
     <a href="#!" class="brand-logo left" style="margin-left: 2%;"><i id="showsidenbutton" class="material-icons">menu</i>
   </a>
   <a href="http://localhost/hrms/" class="brand-logo center">thyssenkrupp Elevators</a>
+  <a href="http://localhost/hrms/" style="margin-left: 93%;" ><?php echo($name) ?></a>
+ <select id="logout"class="dropdown-trigger btn blue darken-1" style="height:62px;width:30px;float:right;" >
+  <option value="profile">Profile</option>
+  <option value="logout">Logout</option>
+</select> 
   </div>
 </nav>
 <br><br>
