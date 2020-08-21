1d4ee40cf4cb9284605255bb0407325f5ad857ff
Pragatigaikwad280 <35777215+Pragatigaikwad280@users.noreply.github.com>
2020-08-21 22:43:42 +0530

    inv-profile nav changes

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,22 +1,22 @@
-$(function() {
-    $('#logout').change(function(){
-      if($(this).val()=="logout"){
-    
+$(function(){
+  $('#logout').change(function(){
+    if($(this).val()=="logout"){
+  
 
-    $.ajax({
-      url:"http://localhost/hrms/api/logout.php",
-      type:"POST",
-      success:function(para){
+  $.ajax({
+    url:"http://localhost/hrms/api/logout.php",
+    type:"POST",
+    success:function(para){
 
-      if(para=="success")
-      {
-     
-      document.location.replace("http://localhost/hrms/index.php")
-      }
-    
-      } 
+    if(para=="success")
+    {
+   
+    document.location.replace("http://localhost/hrms/index.php")
+    }
+  
+    } 
 
 })	  }
-     
-    });
+   
   });
+});
